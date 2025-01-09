7e3df73cb7ceaf48eb64711f9bc2bb0ffee23cec
Jesse Emoghware <168057329+TEKWAVEINC@users.noreply.github.com>
2025-01-09 08:48:26 +0100

    Portfolio removal

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Portfolio from './components/Portfolio';
 import Skills from './components/Skills';
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="portfolio" element={<Portfolio />} />
         <Route path="skills" element={<Skills />} />
       </Route>
     </Routes>
