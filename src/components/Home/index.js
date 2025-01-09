72974406f8e2173f1c630026f9a1e89519278de8
Jesse Emoghware <168057329+TEKWAVEINC@users.noreply.github.com>
2025-01-09 10:29:22 +0100

    Fixes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -9,7 +9,6 @@ const Home = () => {
 
   const nameArray = [' ', 'Z', 'a', 'm', 'z'];
   const hiArray = ['H', 'i', ','];
-  const imArray = ['I', "'", 'm ', " "];
   const systemDeveloperArray = [
     'A',
     ' ',
@@ -56,21 +55,20 @@ const Home = () => {
             <span className="intro">
               <AnimatedLetters
                 letterClass={letterClass}
-                strArray={imArray}
                 delay={1 + hiArray.length * 0.1}
               />
               <AnimatedLetters
                 wrapperClass="nameWrapper"
                 letterClass={letterClass}
                 strArray={nameArray}
-                delay={1 + (hiArray.length + imArray.length) * 0.1}
+                delay={1 + (hiArray.length) * 0.1}
               />
             </span>
             <AnimatedLetters
               letterClass={letterClass}
               strArray={systemDeveloperArray}
               delay={
-                1 + (hiArray.length + imArray.length + " " + nameArray.length) * 0.1
+                1 + (hiArray.length + nameArray.length) * 0.1
               }
             />
           </h1>
